914ff4bf809db984950c624e4d650d07270fbe09
PY2512 <58562352+PY2512@users.noreply.github.com>
2022-02-14 19:09:36 +0200

    finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', startChangeBodyColor);
+refs.stopBtn.addEventListener('click', stopChangeBodyColor);
+refs.stopBtn.setAttribute('disabled', 'disabled');
+let timerId = null;
+
+function startChangeBodyColor() {
+    refs.startBtn.setAttribute('disabled', 'disabled');
+    refs.stopBtn.removeAttribute('disabled', 'disabled');
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function stopChangeBodyColor() {
+    clearInterval(timerId);
+    refs.startBtn.removeAttribute('disabled', 'disabled');
+    refs.stopBtn.setAttribute('disabled', 'disabled');
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
